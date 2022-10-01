19c9810dfe1469006c1126ef4c7d54236deb15b0
Raymond <rjrosales2025@gmail.com>
2022-10-01 15:05:16 -0400

    added file upload button + hello page

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const html = require('html')
 const express = require('express');
 const app = express();
 
@@ -7,13 +8,17 @@ const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
+// app.set('view engine', 'hbs');
+// app.set('view engine', 'html');
 
 app.get('', (req, res) => {
-    res.render('index');
+    // res.render('index');
+    res.sendFile('index.html');
 })
 
 app.get('/hello', (req, res) => {
-    res.render('hello')
+    res.sendFile(publicDirectoryPath + '/hello.html');
+    // res.sendFile('hello.html');
 })
 
 app.listen(port, () => {
