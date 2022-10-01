1e26c99250ce0979111c2a32abcc5bf1c7c4b397
Raymond <rjrosales2025@gmail.com>
2022-10-01 17:52:03 -0400

    file parsing to arrays added

--- public/js/parse.js
+++ public/js/parse.js
@@ -1,7 +1,10 @@
+// import { parse } from 'csv-parse/sync';
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+// const { parse } = require('csv-parse');
+
 const fileUploadForm = document.querySelector('form');
 const fileUploadWidget = document.querySelector('input');
-// const [file] = document.getById('fileupload').files;
-const reader = new FileReader();
 
 fileUploadForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -11,7 +14,26 @@ fileUploadForm.addEventListener('submit', (e) => {
     console.log(file);
     // console.log(typeof file);
 
+    let words_from_file = "";
     file.text().then((result) => {
         console.log(result);
-    })
+        words_from_file = result;
+        // we should probably do something else with the result too
+        console.log('wff', words_from_file)
+        // console.log('BRUH', words_from_file.split('	'))
+        console.log('BRUH', words_from_file.split('\r\n'))
+        const newlineRemoved = words_from_file.split('\r\n')
+        const tabRemoved = [];
+        newlineRemoved.forEach((w) => {
+            tabRemoved.push(w.split('\t'))
+        })
+        console.log(tabRemoved)
+    })  
+    // parse(words_from_file, {
+    //     delimiter: '    '
+    // }, function(err, parsed_result) {
+    //     console.log(parsed_result);
+    // })
+
+    // console.log(big_mood)
 })
