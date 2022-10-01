e4444837cff86437228e255754c99d5f8655f5f3
Raymond <rjrosales2025@gmail.com>
2022-10-01 13:59:51 -0400

    another commit idk

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ app.use(express.static(publicDirectoryPath));
 app.get('', (req, res) => {
     res.render('index');
 })
+
+app.get('/hello', (req, res) => {
+    res.render('hello')
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
