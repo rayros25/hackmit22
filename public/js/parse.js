0b35a0f52a631c2b0c4f6a561c8bc09efafbb0ad
abbyder <abbyder393@gmail.com>
2022-10-01 23:06:49 -0400

    added growth visualization

--- public/js/parse.js
+++ public/js/parse.js
@@ -6,11 +6,34 @@ const flowerZone = document.getElementsByClassName('flower')[0];
 const flashcardText = document.getElementsByClassName('flashcard_content')[0];
 const flashcardBox = document.getElementsByClassName('flashcard')[0];
 
+const growthImage = document.querySelector(".growth-img");
+
 let parsed_words = [];
 let curr = 0;
 let which_side = 0;
 let hasStarted = false;
 let hasBeenFlipped = false;
+
+const flowerGrowth = ["/images/flower1.png", "/images/flower2.png", "/images/flower3.png", "/images/flower4.png", "/images/flower5.png"];
+
+const houseGrowth = ["/images/house1.png", "/images/house2.png", "/images/house3.png", "/images/house4.png", "/images/house5.png"];
+
+const cakeGrowth = ["/images/cake1.png", "/images/cake2.png", "/images/cake3.png", "/images/cake4.png", "/images/cake5.png"];
+
+const fishGrowth = ["/images/fish1.png", "/images/fish2.png", "/images/fish3.png", "/images/fish4.png", "/images/fish5.png"];
+
+const birdGrowth = ["/images/bird1.png", "/images/bird2.png", "/images/bird3.png", "/images/bird4.png", "/images/bird5.png"];
+
+const growthImgArrays = [flowerGrowth, houseGrowth, cakeGrowth, fishGrowth, birdGrowth];
+
+const maxSteps = 15;
+const numGrowthStages = 5;
+let numItemsLeft;
+let cardsCorrect = 0;
+let growthRate;
+let currGrowthStage = 0;
+let currGrowthType;
+
 
 fileUploadForm.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -23,7 +46,7 @@ fileUploadForm.addEventListener('submit', async (e) => {
     // })  
     const result = await file.text();
     parsed_words = parse(result);
-
+    numItemsLeft = parsed_words.length;
     flashcardText.innerHTML = 'Filed uploaded and ready! Click me to start!'
 })
 
@@ -64,17 +87,40 @@ const getNewCard = () => {
     hasBeenFlipped = false;
 }
 
-yesButton.onclick = () => {
+// yesButton.onclick = () => {
+//     console.log("num items left: " + numItemsLeft);
+//     if (hasBeenFlipped) {
+//         cardsCorrect++;
+//         numItemsLeft--;
+// 
+//         updateGrowth();
+//         getNewCard();
+//     }
+// }
+
+function cardCorrect() {
+    console.log("num items left: " + numItemsLeft);
     if (hasBeenFlipped) {
+        cardsCorrect++;
+        numItemsLeft--;
+        
+        updateGrowth();
         getNewCard();
     }
 }
+yesButton.addEventListener("click", cardCorrect);
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") {
+        // console.log("hit right");
+        cardCorrect();
+    }
+});
 
 noButton.onclick = () => {
+    numItemsLeft--;
     if (hasBeenFlipped) {
         getNewCard();
     }
-    flowerZone.setAttribute('src', '../images/bird3.png')
 }
 
 flashcardBox.onclick = () => {
@@ -88,3 +134,49 @@ flashcardBox.onclick = () => {
         hasBeenFlipped = true;
     }
 }
+
+// growth visualization
+
+function setGrowthType() {
+    currGrowthType = Math.floor(Math.random() * growthImgArrays.length);
+}
+
+function setGrowthRate() {
+    if (numItemsLeft < maxSteps) {
+        // number of items market correct required to grow becomes
+        // items / 5
+        growthRate = Math.floor(numItemsLeft / numGrowthStages) + 1;
+    } else {
+        growthRate = 3;
+    }
+}
+
+function updateGrowth() {
+    // set start image after a growth cycle has been completed
+    if (currGrowthStage === 0) {
+        growthImage.setAttribute("src", "/images/start.png");
+    }
+    
+    // reached threshold to update growth display 
+    console.log("cards correct: " + cardsCorrect);
+    console.log("growth rate:" + growthRate); 
+    if (cardsCorrect % growthRate === 0) {
+        growthImage.setAttribute("src", 
+                    growthImgArrays[currGrowthType][currGrowthStage]);
+        // if growth cycle completed, reset
+        if ((currGrowthStage + 1) === numGrowthStages) {
+            currGrowthStage = 0;
+            setGrowthType();
+            setGrowthRate();
+        } else {
+            currGrowthStage++;
+        }
+    }
+    // if all flashcards have been studied, stop growth
+    // if (numItemsLeft === 0) {
+    //     correctBtn.removeEventListener("click", incrementCorrect);
+    // }
+}
+
+setGrowthType();
+setGrowthRate();
